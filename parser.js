e62c28f9718a434607fcc0d47dd03cb1f30bc939
gabrielcc <gabriel.croitoru.cristian@gmail.com>
2011-11-18 16:23:32 +0000

    started implementing parser

---
+++ parser.js
@@ -0,0 +1,55 @@
+// Import the optparse script
+var COMMAND_CHAR = '+';
+var COMMAND_STRING = '/^' + COMMAND_CHAR + '(\w) (.*)$/';
+var parser = {
+    rules : {},
+    on: function(value, fn) {
+        this.rules[value] = fn;
+    },
+    parse: function(message) {
+        console.log(COMMAND_STRING);
+    }
+};
+
+
+// Handle the --include-file switch
+parser.on('include-file', function(name, value) {
+    options.files.push(value);
+});
+
+// Handle the --print switch
+parser.on('print', function(name, value) {
+    console.log('PRINT: ' + (value || 'No message entered'));
+});
+
+// Handle the --date switch
+parser.on('date', function(name, value) {
+    options.date = value;
+});
+
+// Handle the --number switch
+parser.on('number', function(name, value) {
+    options.number = value;
+});
+
+// Handle the --debug switch
+parser.on('debug', function() {
+    options.debug = true;
+});
+
+// Handle the --help switch
+parser.on('help', function() {
+    console.log(parser.toString());
+});
+
+
+function help() {
+    console.log(parser.toString());
+}
+
+/**
+ * Exports section
+ */
+exports.parse = function(message) {
+    parser.parse(message);
+};
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 var yahoo = require('./yahoo.js');
+var parser = require('./parser.js');
 
 yahoo.setCallback(function(message) {
    if (message.message) {
            console.log(message.message.sender + ': ' + message.message.msg);
+           parser.parse(message.message.msg);
         }
 });
 yahoo.login();
